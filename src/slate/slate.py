700cee3fe508a777717bba074a2b724abc553dd0
Gabe Fierro <gtfierro225@gmail.com>
2013-11-06 11:42:56 -0800

    update slate to work with most recent version of pdfminer

--- src/slate/slate.py
+++ src/slate/slate.py
@@ -1,6 +1,7 @@
 from StringIO import StringIO
 
-from pdfminer.pdfparser import PDFParser, PDFDocument
+from pdfminer.pdfdocument import PDFDocument
+from pdfminer.pdfparser import PDFParser
 from pdfminer.pdfinterp import PDFResourceManager
 from pdfminer.pdfinterp import PDFPageInterpreter as PI
 from pdfminer.pdfdevice import PDFDevice
